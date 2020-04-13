af6630937a016da523f40cea293bcc37678fe105
Albina <albafox@yandex.ru>
2020-04-13 20:32:30 +0300

    HW_1_V_2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,23 +2,22 @@ import java.util.Random;
 import java.util.Scanner;
 class MyFirstGame {
 
-  public static void main (String [] args) {
+    public static void main (String [] args) {
+        Random random = new Random();
+        int num = 0 + random.nextInt (101 - 0);
 
-  Random random = new Random();
-  int num = 0 + random.nextInt (101 - 0);
+        System.out.print ("ugadayte chislo ot 0 do 100 ");
+        Scanner aa = new Scanner (System.in);
+        int vvod = aa.nextInt ();
 
-  System.out.print ("ugadayte chislo ot 0 do 100 ");
-    Scanner aa = new Scanner (System.in);
-    int vvod = aa.nextInt ();
+        while (vvod != num) {
 
-    while (vvod != num) {
+        System.out.print ("vvedite ese ras");
+        Scanner bb = new Scanner (System.in);
+        vvod = bb.nextInt ();
 
-    System.out.print ("vvedite ese ras");
-    Scanner bb = new Scanner (System.in);
-    vvod = bb.nextInt ();
-
-    if (vvod==num) {
-      System.out.println (" Vi ugadali ");
+        if (vvod==num) {
+         System.out.println (" Vi ugadali ");
     }
     else if (vvod> num) {
       System.out.println (" Vase chislo bolse zagadannogo ");
