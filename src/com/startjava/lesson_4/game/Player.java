3b82fce670522ad1fa5d4379a364c91c1e9ad627
Albina <albafox@yandex.ru>
2020-05-29 22:40:12 +0300

    HW_4_V5

--- src/com/startjava/lesson_4/game/Player.java
+++ src/com/startjava/lesson_4/game/Player.java
@@ -16,7 +16,11 @@ public class Player {
     }
 
     public int getNumber() {
-        return enteredNums[attempt -1];
+        return enteredNums[attempt - 1];
+    }
+
+    public void setNumber(int guessNumber) {
+        enteredNums[attempt - 1] = guessNumber;
     }
 
     public int getAttempt() {
@@ -25,10 +29,6 @@ public class Player {
 
     public void setAttempt(int attempt) {
         this.attempt = attempt;
-    }
-
-    public void setNumber(int guessNumber) {
-        enteredNums[attempt -1] = guessNumber;
     }
 
     public int[] getEnteredNums() {
